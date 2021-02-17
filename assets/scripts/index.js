97f2fa9ca8a70f84d3070684f0c4ae80d924c3dc
dimitar-4 <dcholakov4@gmail.com>
2021-02-17 22:14:14 +0000

    Add style to the navbar and background image to home page

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,4 +4,5 @@ tl.to(".welcome-text", { y: "0%", duration: 1, stagger: 0.25 });
 tl.to(".slider", { y: "-100%", duration: 1.5, delay: 1 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".home-name", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 tl.fromTo(".home-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
