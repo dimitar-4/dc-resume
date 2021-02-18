7981a2555dc299e7687a75d155691314c4e71bed
dimitar-4 <dcholakov4@gmail.com>
2021-02-18 12:08:39 +0000

    Add list of links to the home page

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,3 +6,4 @@ tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".home-name", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 tl.fromTo(".home-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.fromTo(".home-list", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
