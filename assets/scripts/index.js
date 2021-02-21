2554e13b822464fbf9f1821c2251846fc17c655e
dimitar-4 <dcholakov4@gmail.com>
2021-02-21 23:12:09 +0000

    Change the timeline durations for the home page content

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,10 +6,10 @@ tl.to(".welcome-text", { y: "0%", duration: 1, stagger: 0.25 });
 tl.to(".slider", { y: "-100%", duration: 1.5, delay: 1 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo(".nav-home", { opacity: 0 }, { opacity: 1, duration: 1 });
-tl.fromTo(".home-name", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
-tl.fromTo(".home-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
-tl.fromTo(".contact-content", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=4");
+tl.fromTo(".home-name", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=0.25");
+tl.fromTo(".home-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=0.5");
+tl.fromTo(" .home-list", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=0.75");
 
 // Contact Page Content
 
-tl.fromTo(".home-list", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.fromTo(".contact-content", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=5");
